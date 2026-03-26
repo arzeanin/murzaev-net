5a61b58043d5209d31b25eb3be999f69c72ba209
Vadim <arzeanin@gmail.com>
2026-03-26 11:40:19 +0200

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,13 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
-  // Настройки для корректного отображения картинок
   images: {
-    localPatterns: [
-      {
-        pathname: '/api/media/file/**',
-      },
-    ],
+    localPatterns: [{ pathname: '/api/media/file/**' }],
   },
-  // Настройка SASS для Payload
   sassOptions: {
     includePaths: ['./node_modules'],
   },
-  // Настройки Webpack
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
-    return webpackConfig
-  },
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig)
